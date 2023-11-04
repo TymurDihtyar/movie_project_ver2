f645cee7fbd24de519e6f09e46d636ca94b3b175
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-11-05 01:32:49 +0200

    moviesPage

--- src/Components/MoviesContainer/Movie.tsx
+++ src/Components/MoviesContainer/Movie.tsx
@@ -1,6 +1,9 @@
-import {FC, PropsWithChildren, useState} from 'react';
+import {FC, PropsWithChildren} from 'react';
+import {Rating} from "@mui/material";
+
 import {IMovie} from "../../interfaces";
 import {urls} from "../../constants/urls";
+import css from './Movie.module.css'
 
 interface IProps extends PropsWithChildren {
     item:IMovie
@@ -10,9 +13,9 @@ const Movie:FC<IProps> = ({item}) => {
     const {id, original_title, poster_path, vote_average} = item;
 
     return (
-        <div>
+        <div className={css.movie}>
             <img src={`${urls.poster(poster_path)}`} alt="original_title"/>
-
+            <Rating name="customized-10" defaultValue={vote_average} precision={0.1} max={10} />
             <h3>{original_title}</h3>
         </div>
     );
--- src/Components/MoviesContainer/Movies.tsx
+++ src/Components/MoviesContainer/Movies.tsx
@@ -1,6 +1,7 @@
 import {FC, PropsWithChildren} from 'react';
 import {IMovie} from "../../interfaces";
 import {Movie} from "./Movie";
+import css from './Movies.module.css'
 
 interface IProps extends PropsWithChildren {
     movies: IMovie[]
@@ -8,7 +9,7 @@ interface IProps extends PropsWithChildren {
 
 const Movies:FC<IProps> = ({movies}) => {
     return (
-        <div>
+        <div className={css.allMovies}>
             {movies.map(item => <Movie key={item.id} item={item}/>)}
         </div>
     );
