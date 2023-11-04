41fa264c89fd51f863a432337cf3b29f708e78b3
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-11-04 20:28:53 +0200

    urls

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,6 +1,16 @@
 const baseURL = 'https://api.themoviedb.org/3';
+const movies = '/discover/movie';
+const genre = '/genre/movie/list';
+const poster = 'https://image.tmdb.org/t/p/w500';
+const search = '/search/keyword';
+const character = '/movie';
 
+const urls = {
+    movies,
+    genre,
+    poster: (key: string) => `${poster}/${key}`,
+    search,
+    character: (id:number) => `${character}/${id}/credits`
+}
 
-
-
-export {baseURL}
+export {baseURL, urls}
