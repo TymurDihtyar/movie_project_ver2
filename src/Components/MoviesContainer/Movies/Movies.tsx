bf21b099e5c8af0015ab9e54e875b3adf160b147
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-11-05 04:02:16 +0200

    moviesPage

--- src/Components/MoviesContainer/Movies/Movies.tsx
+++ src/Components/MoviesContainer/Movies/Movies.tsx
@@ -1,4 +1,5 @@
 import {FC, PropsWithChildren} from 'react';
+import {SetURLSearchParams} from "react-router-dom";
 
 import {IMovie} from "../../../interfaces";
 import {Movie} from "../Movie";
@@ -6,13 +7,34 @@ import css from './Movies.module.css'
 
 interface IProps extends PropsWithChildren {
     movies: IMovie[]
+    prevNext: { prev: string; next: string }
+    setQuery: SetURLSearchParams
+    page: string
 }
 
-const Movies:FC<IProps> = ({movies}) => {
+const Movies: FC<IProps> = ({movies, prevNext, setQuery, page}) => {
+
+    function prevHandler() {
+        const prevPage = +page - 1;
+        setQuery({page: prevPage.toString()});
+    }
+
+    function nextHandler() {
+        const nextPage = +page + 1;
+        setQuery({page: nextPage.toString()});
+    }
+
+    console.log(prevNext.prev);
+    console.log(prevNext.next);
+
     return (
         <div className={css.allMovies}>
             {movies.map(item => <Movie key={item.id} item={item}/>)}
-
+            <div className={css.butt}>
+                <button disabled={!prevNext.prev} onClick={prevHandler}>prev</button>
+                <p>{`${page} from ${prevNext.next}`}</p>
+                <button disabled={!prevNext.next} onClick={nextHandler}>next</button>
+            </div>
         </div>
     );
 };
--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -1,4 +1,4 @@
-import {Outlet, useSearchParams} from "react-router-dom";
+import {useSearchParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 
 import {moviesService} from "../services";
@@ -7,19 +7,20 @@ import {Movies} from "../Components";
 
 const MoviesPage = () => {
     const [movies, setMovies] = useState<IMovie[]>([])
-    const [query, setQuery] = useSearchParams({page:'1'});
+    const [query, setQuery] = useSearchParams({page: '1'});
+    const [prevNext, setPrevNext] = useState<{ prev: string; next: string }>({prev: null, next: null})
+    const page = query.get('page')
 
     useEffect(() => {
-        moviesService.getAll('1').then(({data}) => {
+        moviesService.getAll(page).then(({data}) => {
             setMovies(data.results)
+            setPrevNext({prev: `${data.page}`, next: `${data.total_pages}`})
         })
-    }, []);
-
+    }, [page]);
 
     return (
         <div>
-            <Movies movies={movies}/>
-            <Outlet/>
+            <Movies movies={movies} setQuery={setQuery} prevNext={prevNext} page={page}/>
         </div>
     );
 };
