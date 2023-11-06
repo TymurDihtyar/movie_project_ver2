29110ae38b65840dc2d5a8cb176ba9cc11669c1f
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-11-06 15:14:09 +0200

    moviePage

--- src/services/characterService.ts
+++ src/services/characterService.ts
@@ -1,7 +1,7 @@
 import {axiosService} from "./axiosService";
 import {urls} from "../constants/urls";
 import {IRes} from "../types";
-import {IChar} from "../interfaces/charactersInterface";
+import {IChar} from "../interfaces";
 
 const characterService = {
     getById: (id: string): IRes<IChar> => axiosService.get(urls.characters(id))
--- src/services/moviesService.ts
+++ src/services/moviesService.ts
@@ -2,8 +2,8 @@ import {axiosService} from "./axiosService";
 
 import {urls} from "../constants/urls";
 import {IRes} from "../types";
-import {IData} from "../interfaces";
-import {IOneMove} from "../interfaces/oneMovieInteface";
+import {IData, IOneMove} from "../interfaces";
+
 
 const moviesService = {
     getAll: (page:string): IRes<IData> => axiosService.get(urls.movies, {params:{page}}),
