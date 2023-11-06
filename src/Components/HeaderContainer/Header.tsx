7fab8de97ed21eb1ef763337c451ca829d890d15
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-11-07 01:08:42 +0200

    Genres

--- src/Components/HeaderContainer/Header.tsx
+++ src/Components/HeaderContainer/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
             <div className={css.logo}>The MovieDB</div>
             <div className={css.mid}>
                 <NavLink to={'movies'}>Movies</NavLink>
-                <NavLink to={'genres'}>Genres</NavLink>
+                <NavLink to={'genres/:idGenres'}>Genres</NavLink>
                 <NavLink to={'search'}>Search</NavLink>
             </div>
             <div className={css.user}>
