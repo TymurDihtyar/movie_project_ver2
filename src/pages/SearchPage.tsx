90f57b0b81ee770974b9d1b60ac44baeed61f0f9
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-11-07 15:40:40 +0200

    SearchPage.tsx

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -6,7 +6,7 @@ import {IMovie} from "../interfaces";
 import {searchService} from "../services";
 
 const SearchPage = () => {
-    const [KeyWord, setKeyWord] = useState<string>( 'dead')
+    const [KeyWord, setKeyWord] = useState<string>( 'porn')
     const [moviesKeyWord, setMoviesKeyWord] = useState<IMovie[]>([])
     const [query, setQuery] = useSearchParams({page: '1'});
     const page = query.get('page') ? query.get('page') : '1'
